727b1a507f0aa79ba596e94d6d6e3bb20bb0d76c
ADITYA-dev02 <adityaraturi02@gmail.com>
2025-05-04 12:44:23 +0530

    Add files via upload

---
+++ m5.py
@@ -0,0 +1,11 @@
+from call import add1, sub1, mul1, div1, mod1, pow1
+
+a = int(input("enter number : "))
+b = int(input("enter number : "))
+
+print("Addition:", add1(a, b))
+print("Subtraction:", sub1(a, b))
+print("Multiplication:", mul1(a, b))
+print("Division:", div1(a, b))
+print("Modulo:", mod1(a, b))
+print("Power:", pow1(a, b))
