39d5c8c81cf9eeebe04e7f6b14c74bb19e8f9a62
ADITYA-dev02 <adityaraturi02@gmail.com>
2025-05-04 12:54:26 +0530

    Delete zero.py

--- zero.py
+++
@@ -1,9 +0,0 @@
-a = float(input("Enter numerator: "))
-b = float(input("Enter denominator: "))
-
-try:
-    result = a / b
-except ZeroDivisionError:
-    result = 'undefined'
-
-print("Result:", result)
